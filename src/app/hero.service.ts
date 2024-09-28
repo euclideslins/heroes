d8217da179020f9482d330e128d4ce1f0096ca54
Euclides Vasconcelos <evascon@motorola.com>
2024-09-27 20:11:36 -0400

    Aula 27/09

---
+++ src/app/hero.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HEROES } from './mock-heroes';
+import { Hero } from './hero';
+import { catchError, Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(
+    private messageService: MessageService,
+    private httpClient: HttpClient
+  ) {}
+
+  heroesUrl = 'api/heroes'
+
+  getHeroes(): Observable<Hero[]> {
+    return this.httpClient.get<Hero[]>(this.heroesUrl).pipe(
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    };
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`
+    return this.httpClient.get<Hero>(url).pipe(
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    )
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.httpClient.put(this.heroesUrl, hero).pipe(
+      catchError(this.handleError<any>('updateHero'))
+    )
+  }
+
+
+
+
+}
